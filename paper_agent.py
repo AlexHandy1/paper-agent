7574a45dd4a3add97b9b0611dc574ad52ece48ca
AlexHandy1 <handy437@gmail.com>
2023-05-19 21:37:59 +0100

    moved process to config

--- paper_agent.py
+++ paper_agent.py
@@ -1,7 +1,6 @@
 #next steps
-    #add loop through key alert terms in bash script (which is why need llm reviewer optional / elsewhere)
-    #add bioxriv, medxriv (and google scholar)
     #setup cronjob to run once a week on Fridays
+    #add bioxriv, medxriv (and google scholar)
     #try other LLMs
 
 import article_consolidator as ac
@@ -9,8 +8,6 @@ import llm_reviewer as lr
 from utils import * 
 import datetime
 from datetime import datetime
-# import faiss
-# from sentence_transformers import SentenceTransformer
 import sys
 import config as cf
 
@@ -20,71 +17,63 @@ llm_agent = lr.LLMReviewer(system_prompt)
 today = datetime.now()
 today_str = today.strftime("%d_%m_%Y_%H_%M_%S")
 
-if len(sys.argv) < 2:
-    raise Exception("Error, no query provided")
-elif len(sys.argv) < 3:
-    print("Query only, no LLM topic check")
-    query = sys.argv[1]
-    topic_keyword = "Not provided"
+queries = cf.query_config["queries"]
+queries_topic_checks = cf.query_config["topic_checks"]
+max_results = cf.query_config["max_results"]
 
+run_topic_check = cf.llm_config["run_topic_check"]
+
+for idx, query in enumerate(queries):
     print("Query: ", query)
-    print("Topic keyword: ", topic_keyword)
-else:
-    print("Query and LLM topic check")
-    query = sys.argv[1]
-    topic_keyword = sys.argv[2]
+    article_consolidator = ac.ArticleConsolidator(query, max_results, min_date='2020/01/01', search_date=today_str)
+    articles = article_consolidator.consolidate()
 
-    print("Query: ", query)
-    print("Topic keyword: ", topic_keyword)
+    labelled_articles = []
 
-max_results = 10
-article_consolidator = ac.ArticleConsolidator(query, max_results, min_date='2020/01/01', search_date=today_str)
-articles = article_consolidator.consolidate()
+    #get existing titles to prevent duplicates
+    credentials_key_path = cf.setup_config["credentials_key_path"]
+    gsheet_key = cf.setup_config["gsheet_key"]
+    gsheet_tab_name = cf.setup_config["gsheet_tab_name"]
+    tgt_col_num = cf.setup_config["target_col_num"]
 
-labelled_articles = []
+    current_paper_titles = get_worksheet_title_list(gsheet_key, credentials_key_path, gsheet_tab_name, tgt_col_num)
 
-#get existing titles to prevent duplicates
-credentials_key_path = cf.setup_config["credentials_key_path"]
-gsheet_key = cf.setup_config["gsheet_key"]
-gsheet_tab_name = 'To Read - AgentList'
-tgt_col_num = 1
+    # Print the consolidated articles
+    for article in articles:
+        print("Title: ", article['Title'])
+        print("Abstract: ", article['Abstract'])
+        print("Published: ", article['Published'])
+        print("Link: ", article['Link'])
+        print("Source: ", article['Source'])
 
-current_paper_titles = get_worksheet_title_list(gsheet_key, credentials_key_path, gsheet_tab_name, tgt_col_num)
+        if article["Title"] in current_paper_titles:
+            print("Not added: article title already in list")
+        elif run_topic_check:
+            query_topic_check = queries_topic_checks[idx]
+            print("Query topic check: ", query_topic_check)
 
+            #review the content with ChatGPT
+            llm_topic_mention_prompt_template = """ 
 
-# Print the consolidated articles
-for article in articles:
-    print("Title: ", article['Title'])
-    print("Abstract: ", article['Abstract'])
-    print("Published: ", article['Published'])
-    print("Link: ", article['Link'])
-    print("Source: ", article['Source'])
+            Does the abstract below mention {topic}?
 
-    if article["Title"] in current_paper_titles:
-        print("Not added: article title already in list")
-    elif topic_keyword == "Not provided":
-        print("Topic check query parameter not provided - article added without a check")
-        article["Topic Check"] = "N/A"
-        article["Topic Check Query"] = "N/A"
-        labelled_articles.append(article)
-    else:
-        #review the content with ChatGPT
-        llm_topic_mention_prompt_template = """ 
+            If the answer is yes, please respond with "Yes: <Example sentence that mentions topic>"
+            If the answer is no, please respond with "No"
 
-        Does the abstract below mention {topic}?
+            {abstract}
 
-        If the answer is yes, please respond with "Yes: <Example sentence that mentions topic>"
-        If the answer is no, please respond with "No"
+            """
+            llm_topic_mention_prompt = llm_topic_mention_prompt_template.format(topic=query_topic_check, abstract=article['Abstract'])
+            llm_topic_review = llm_agent.chat_without_memory(llm_topic_mention_prompt)
+            print("LLM Agent topic review: ", llm_topic_review)
+            print('-' * 80)
+            article["Topic Check"] = llm_topic_review
+            article["Topic Check Query"] = query_topic_check
+            labelled_articles.append(article)
+        else:
+            print("Topic check query not run")
+            article["Topic Check"] = "N/A"
+            article["Topic Check Query"] = "N/A"
+            labelled_articles.append(article)
 
-        {abstract}
-
-        """
-        llm_topic_mention_prompt = llm_topic_mention_prompt_template.format(topic=topic_keyword, abstract=article['Abstract'])
-        llm_topic_review = llm_agent.chat_without_memory(llm_topic_mention_prompt)
-        print("LLM Agent topic review: ", llm_topic_review)
-        print('-' * 80)
-        article["Topic Check"] = llm_topic_review
-        article["Topic Check Query"] = topic_keyword
-        labelled_articles.append(article)
-
-add_articles_to_gsheet(labelled_articles, gsheet_key, credentials_key_path, gsheet_tab_name)
+    add_articles_to_gsheet(labelled_articles, gsheet_key, credentials_key_path, gsheet_tab_name)
